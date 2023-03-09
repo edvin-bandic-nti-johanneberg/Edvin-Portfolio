5d317086537eae32502e7623f94f505683012293
edvin-bandic-nti-johanneberg <edvin.bandic@elev.ga.ntig.se>
2023-03-09 14:51:14 +0100

    fortsättning med kodning
    nu är jag nästan klar med min header

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click", mobileMenu);
-
-function mobileMenu(click) {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
-
-
-const navLink = document.querySelectorAll(".nav-link");
-
-navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-function closeMenu(click) {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+    x.className += " responsive";
+    } else {
+    x.className = "topnav";
+    }
 }
